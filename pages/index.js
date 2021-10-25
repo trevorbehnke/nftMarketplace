91f2abfb8cc92f4135eadddc02703a69fcc3a803
trevor <me@trevorbehnke.com>
2021-10-25 15:48:56 -0700

    feat: :sparkles: configure creator-dashboard page

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
               </div>
               <div className="p-4 bg-black">
                 <p className="text-2xl mb-4 font-bold text-white">
-                  {nft.price} ETH
+                  {nft.price} Matic
                 </p>
                 <button
                   className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
