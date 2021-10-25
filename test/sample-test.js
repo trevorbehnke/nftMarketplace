a50f87b7f23d9a5de17e00f5544d81dfe5acdf41
trevor <me@trevorbehnke.com>
2021-10-25 10:33:44 -0700

    feat: :sparkles: test update

--- test/sample-test.js
+++ test/sample-test.js
@@ -34,7 +34,21 @@ describe("Greeter", function () {
       .connect(buyerAddress)
       .createMarketSale(nftContractAddress, 1, { value: auctionPrice });
 
-    const items = await market.fetchMarketItems();
+    let items = await market.fetchMarketItems();
+
+    items = await Promise.all(
+      items.map(async (i) => {
+        const tokenUri = await nft.tokenURI(i.tokenId);
+        let item = {
+          price: i.price.toString(),
+          tokenId: i.tokenId.toString(),
+          seller: i.seller,
+          owner: i.owner,
+          tokenUri,
+        };
+        return item;
+      })
+    );
 
     console.log("items: ", items);
   });
